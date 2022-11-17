5820de159356138d5c22512621f42e76e4abd9ee
Carlos Fonteles <karllos1982@hotmail.com>
2022-11-17 12:38:56 -0300

    Desenvolvimento do modulo Membership (cont.)
    Continuação do desenvolvimento do módulo Membership.

--- Project/GW.Membership/Domain/InstanceDomain.cs
+++ Project/GW.Membership/Domain/InstanceDomain.cs
@@ -4,6 +4,7 @@ using GW.Membership.Contracts;
 using GW.Membership.Contracts.Domain;
 using GW.Membership.Models;
 using GW.Membership.Contracts.Data;
+using GW.Helpers;
 
 namespace GW.Membership.Domain
 {
@@ -21,53 +22,185 @@ namespace GW.Membership.Domain
 
         public InstanceModel Get(InstanceParam param)
         {
-            throw new NotImplementedException();
+            InstanceModel ret = null;
+
+            ret = RepositorySet.Instance.Read(param); 
+            
+            return ret;
         }
 
         public List<InstanceList> List(InstanceParam param)
         {
-            throw new NotImplementedException();
+            List<InstanceList> ret = null;
+
+            ret = RepositorySet.Instance.List(param);           
+
+            return ret;
         }
 
         public List<InstanceSearchResult> Search(InstanceParam param)
         {
-            throw new NotImplementedException();
+            List<InstanceSearchResult> ret = null;
+
+            ret = RepositorySet.Instance.Search(param);
+
+            return ret;
         }
 
         public OperationStatus Set(InstanceModel model, object userid)
         {
-            throw new NotImplementedException();
+            OperationStatus ret = new OperationStatus(true);
+            OPERATIONLOGENUM operation = OPERATIONLOGENUM.INSERT;
+
+            ret = EntryValidation(model);
+
+            if (ret.Status)
+            {
+
+                InstanceModel old 
+                    = RepositorySet.Instance.Read(new InstanceParam() { pInstanceID = model.InstanceID });
+
+                if (old == null)
+                {
+                    ret = InsertValidation(model);
+
+                    if (ret.Status)
+                    {
+                        model.CreateDate = DateTime.Now;
+                        ret = RepositorySet.Instance.Create(model);
+                    }
+                }
+                else
+                {
+                    model.CreateDate = old.CreateDate;
+                    operation = OPERATIONLOGENUM.UPDATE;
+
+                    ret = UpdateValidation(model);
+
+                    if (ret.Status)
+                    {
+                        ret = RepositorySet.Instance.Update(model);
+                    }
+
+                }
+
+                if (ret.Status && userid != null)
+                {
+                    //RepositorySet.Instance.Context.
+                    //    .RegisterDataLog(userid.ToString(), operation, "SYSINSTANCE",
+                    //  obj.InstanceID.ToString(), old, obj);
+
+                    ret.Returns = model;
+                }
+
+            }     
+
+            return ret;
         }
 
         public void FillChields(ref InstanceModel obj)
         {
-            throw new NotImplementedException();
+            
         }
 
         public OperationStatus Delete(InstanceModel model, object userid)
         {
-            throw new NotImplementedException();
+            OperationStatus ret = new OperationStatus(true);
+
+            InstanceModel old 
+                = RepositorySet.Instance.Read(new InstanceParam() { pInstanceID = model.InstanceID });
+
+            if (old != null)
+            {
+                ret = DeleteValidation(model);
+
+                if (ret.Status)
+                {
+                    ret = RepositorySet.Instance.Delete(model);
+                }
+            }
+            else
+            {
+                ret.Status = false;
+                ret.Error = new System.Exception(GW.Localization.GetItem("Record-NotFound").Text);
+
+            }           
+
+            return ret;
         }
 
-        public OperationStatus DeleteValidation(InstanceModel obj)
-        {
-            throw new NotImplementedException();
-        }
 
         public OperationStatus EntryValidation(InstanceModel obj)
         {
-            throw new NotImplementedException();
-        }
+            OperationStatus ret = null;
+
+            ret = PrimaryValidation.Execute(obj, new List<string>());
+
+            if (!ret.Status)
+            {
+                ret.Error = new Exception(GW.Localization.GetItem("Validation-Error").Text);
+            }
+
+            Context.ExecutionStatus = ret; 
+
+            return ret;
+        }           
              
         public OperationStatus InsertValidation(InstanceModel obj)
         {
-            throw new NotImplementedException();
+            OperationStatus ret = new OperationStatus(true);
+            InstanceParam param = new InstanceParam()
+            {
+                pInstanceName = obj.InstanceName
+            };
+
+            List<InstanceList> list
+                = RepositorySet.Instance.List(param);
+
+            if (list != null)
+            {
+                if (list.Count > 0)
+                {
+                    ret.Status = false;
+                    ret.Error = new Exception(GW.Localization.GetItem("Validation-Unique-Value").Text);
+                }
+            }
+
+            Context.ExecutionStatus = ret;
+
+            return ret;
         }
             
         public OperationStatus UpdateValidation(InstanceModel obj)
         {
-            throw new NotImplementedException();
+            OperationStatus ret = new OperationStatus(true);
+            InstanceParam param = new InstanceParam() { pInstanceName = obj.InstanceName };
+            List<InstanceList> list
+                = RepositorySet.Instance.List(param);
+
+            if (list != null)
+            {
+                if (list.Count > 0)
+                {
+                    if (list[0].InstanceID != obj.InstanceID)
+                    {
+                        ret.Status = false;
+                        ret.Error = new Exception(GW.Localization.GetItem("Validation-Unique-Value").Text);
+                    }
+                }
+            }
+
+            Context.ExecutionStatus = ret;
+
+            return ret;
+
         }
+
+        public OperationStatus DeleteValidation(InstanceModel obj)
+        {
+            return new OperationStatus(true); 
+        }
+
 
     }
 }
